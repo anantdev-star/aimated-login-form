acd849ac58ad9ee14cdc74e05abe8ea396b352b4
anantdev-star <62455447+anantdev-star@users.noreply.github.com>
2020-04-18 11:40:12 +0530

    Add files via upload
    anumated login form using html,css and javascript

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const inputs = document.querySelectorAll('.input');
+
+function focusFunc(){
+  Let parent = this.parentNode.parentNode;
+  parent.classList.add('focus');
+
+}
+function blurFunc(){
+  Let parent = this.parentNode.parentNode;
+  if(this.value == ""){
+    parent.classList.remove('focus');
+  }
+}
+
+inputs.forEach(input => {
+  input.addEventListener('focus',focusFunc);
+  input.addEventListener('blur',blurFunc);
+});
